06d055e3883eb6c3ac212ec405bd11319cd3011d
ellow0rld <116413038+ellow0rld@users.noreply.github.com>
2025-04-05 14:24:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,16 +2,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "app_server" {
-  ami           = "ami-0db4215299a464a09"
-  instance_type = "t2.micro"
-  key_name      = "aws-terraform"
-
-  tags = {
-    Name = "AppServer"
-  }
-}
-
 resource "aws_security_group" "web_sg" {
   name        = "web-security-group"
   description = "Allow HTTP and SSH traffic"
@@ -35,6 +25,29 @@ resource "aws_security_group" "web_sg" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0db4215299a464a09"
+  instance_type = "t2.micro"
+  key_name      = "aws-terraform"
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo yum update -y
+              sudo yum install -y git httpd
+              systemctl start httpd
+              systemctl enable httpd
+              cd /var/www/html
+              git clone git@github.com:ellow0rld/Time-Zone-Converter.git site
+              cp -r site/* .
+              rm -rf site
+            EOF
+
+  tags = {
+    Name = "StaticHTMLServer"
   }
 }
 
